6073ac65a1b2909029b4cca59a8e7596c0983a9b
Thane5 <13683581+Thane5@users.noreply.github.com>
2022-02-15 12:11:24 +0100

    Added scene for graphics testing
    Has buttons for Shadows, FXAA, pixelatione effect

---
+++ Assets/Graphics Tests/test_settings_manager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class test_settings_manager : MonoBehaviour
+{
+    //Variables
+    public PostProcessLayer localPostProcessLayer;
+    public PixelateEffect cameraObject;
+    //public bool shadowsEnabled
+
+    //Function to change pixel size with UI buttons
+    public void SetPixelScale(float overridePixelScale)
+    {
+        cameraObject.pixelScale = overridePixelScale;
+        print(overridePixelScale);
+    }
+
+    public void ToggleAA(bool antiAliasingEnabled)
+    {
+        if (antiAliasingEnabled)
+        {
+            localPostProcessLayer.antialiasingMode = PostProcessLayer.Antialiasing.SubpixelMorphologicalAntialiasing;
+        }
+        if (!antiAliasingEnabled)
+        {
+            localPostProcessLayer.antialiasingMode = PostProcessLayer.Antialiasing.None;
+        }
+        //localPostProcessLayer.antialiasingMode = PostProcessLayer.Antialiasing.SubpixelMorphologicalAntialiasing;
+        //print("AA = " + antiAliasingEnabled);
+    }
+
+    public void ToggleShadows(bool shadowsEnabled)
+    {
+        if (shadowsEnabled)
+        {
+            QualitySettings.shadows = ShadowQuality.All;
+        }
+        if (!shadowsEnabled)
+        {
+            QualitySettings.shadows = ShadowQuality.Disable;
+        }
+    }
+}
