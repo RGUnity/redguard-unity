650597c3e68826316d4addb9f9b6eabeae4fa7a3
anchor <anchorlightforge@gmail.com>
2023-01-21 23:52:22 -0500

    Smacker format sandbox
    Played around with Smacker video format header, not ready for use at this time.

--- Assets/Scripts/RGFileImport/RGVidImport/SmackerHeader.cs
+++ Assets/Scripts/RGFileImport/RGVidImport/SmackerHeader.cs
@@ -1,17 +1,18 @@
 public class SmackerHeader
 {
-    uint Signature;
+    //All info courtesy of https://wiki.multimedia.cx/index.php?title=Smacker
+    uint Signature; // SMK2 or SMK4
     uint Width {get; set;}
     uint Height{get; set;}
-    uint Frames{get;set;}
-    float FrameRate{get;set;}
-    uint Flags{get;set;}
-    uint[] AudioSize{get;set;}
-    uint TreesSize{get;set;}
-    uint MMap_Size{get;set;}
-    uint MClr_Size{get;set;}
-    uint Full_Size{get;set;}
-    uint Type_Size{get;set;}
-    uint[] AudioRate{get;set;}
+    uint Frames{get;set;} //Extra "ring" frame is not counted
+    float FrameRate{get;set;} //fps = 1000/Mathf.Abs(FrameRate) or if 0, fps = 10
+    uint Flags{get;set;} // 0
+    uint[] AudioSize{get;set;} //Size of largest buffer (bytes) per audio track (up to 8)
+    uint TreesSize{get;set;} //Size of bytes in huffman trees
+    uint MMap_Size{get;set;} //Huffman data
+    uint MClr_Size{get;set;}//huffman data
+    uint Full_Size{get;set;}//huffman data
+    uint Type_Size{get;set;}//huffman data
+    uint[] AudioRate{get;set;} 
     uint Dummy{get;set;}
 }
