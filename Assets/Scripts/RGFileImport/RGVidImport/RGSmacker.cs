53fe7ea29231c5907652534bdd28197514684645
anchor <anchorlightforge@gmail.com>
2023-01-23 00:29:43 -0500

    Update RGSmacker.cs
    Further progress on the Smacker format header and a void to run through the entire process. Still not usable.

--- Assets/Scripts/RGFileImport/RGVidImport/RGSmacker.cs
+++ Assets/Scripts/RGFileImport/RGVidImport/RGSmacker.cs
@@ -32,8 +32,29 @@ public class RGSmacker
     //Convert to bytes then convert to colors then output to Unity
     string filepath = "D:" + System.IO.Path.DirectorySeparatorChar;
 
+    int[] FrameSizes;
+
     SmackerHeader dataHeader;
     BinaryReader binaryReader;
+
+
+
+    void LoadFile()
+    {
+        if(File.Exists(filepath))
+        {
+            binaryReader = new BinaryReader(File.OpenRead(filepath));
+            dataHeader = GetHeader(binaryReader);
+        }
+        else throw new System.Exception("The file "+filepath+ " could not be found. The video was not loaded.");
+        dataHeader = GetHeader(binaryReader);
+        ReadFrameSizes();
+        ReadFrameTypes();
+        GetHuffmanTrees();
+        GetFramesData();
+    }
+
+
     public struct PaletteChunk
     {
         byte Length;
@@ -46,11 +67,12 @@ public class RGSmacker
         byte[] Data;
     }
 
-    void GetFramerate()
+    float GetFramerate()
     {
         float fps = 10;
         if(dataHeader.FrameRate != 0)
             fps = 1000f / Mathf.Abs(dataHeader.FrameRate);
+        return fps;
     }
 
     private SmackerHeader GetHeader(BinaryReader binaryReader)
@@ -59,27 +81,69 @@ public class RGSmacker
         var newHeader = new SmackerHeader
         {
             Signature = binaryReader.ReadUInt32(), // SMK2 or SMK4
-            Width = binaryReader.ReadUInt32(),
-            Height = binaryReader.ReadUInt32(),
-            Frames = binaryReader.ReadUInt32(), //Extra "ring" frame is not counted
+            Width = binaryReader.ReadUInt32(), //most videos will return 640
+            Height = binaryReader.ReadUInt32(), //most videos will return 240
+            Frames = binaryReader.ReadUInt32(), //Extra "ring" frame is not counted; Intro is about 6895 frames
             FrameRate = binaryReader.ReadUInt32(), //fps = 1000/Mathf.Abs(FrameRate) or if 0, fps = 10
             Flags = binaryReader.ReadUInt32(),// 0
-            AudioSize = new uint[1]{binaryReader.ReadUInt32()},//Size of largest buffer (bytes) per audio track (up to 8)... this isn't how it works and needs to be fixed
+            AudioSize = new uint[7]{binaryReader.ReadUInt32(),binaryReader.ReadUInt32(),binaryReader.ReadUInt32(),binaryReader.ReadUInt32(),binaryReader.ReadUInt32(),binaryReader.ReadUInt32(),binaryReader.ReadUInt32()},//Size of largest buffer (bytes) per audio track (up to 8)... this isn't how it works and needs to be fixed
             TreesSize = binaryReader.ReadUInt32(),//Size of bytes in huffman trees
             MMap_Size = binaryReader.ReadUInt32(),//Huffman data
             MClr_Size = binaryReader.ReadUInt32(),//huffman data
             Full_Size = binaryReader.ReadUInt32(),//huffman data
             Type_Size = binaryReader.ReadUInt32(),//huffman data
-            AudioRate = new uint[1]{binaryReader.ReadUInt32()},
+            AudioRate = new uint[7]{binaryReader.ReadUInt32(),binaryReader.ReadUInt32(),binaryReader.ReadUInt32(),binaryReader.ReadUInt32(),binaryReader.ReadUInt32(),binaryReader.ReadUInt32(),binaryReader.ReadUInt32()},
             Dummy = binaryReader.ReadUInt32(),
         };
 
         return (SmackerHeader)newHeader;
     }
 
-    void GetSMKInfo()
+    private void ReadFrameSizes()
     {
-        
+        //should be done after reading Header
+        FrameSizes = new int[dataHeader.Frames];
+        for (int i = 0; i < dataHeader.Frames; i++)
+        {
+            FrameSizes[i] = (int)binaryReader.ReadUInt32();
+        }
+    }
+    int[] FrameTypes;
+    private void ReadFrameTypes()
+    {
+        for (int i = 0; i < dataHeader.Frames;i++)
+        {
+            FrameTypes[i] = (int)binaryReader.ReadByte();
+
+        //do after reading framesizes
+        //if bit 0 != 0, palette record is included
+        //if bit 1 != 0, audio data is included for track 0
+        //other six bits are unused for these videos because there's only one audio track included
+            
+        }
+    }
+
+    private void GetHuffmanTrees()
+    {
+        //Includes tree data for the four decoding tables listed in the header
+         
+    }
+
+    private void GetFramesData()
+    {
+
+    }
+    int version;
+
+    void GetSMKVersion()
+    {
+        version = dataHeader.Signature switch
+        {
+            0x534D4B32 => 2, //SMK2
+            0x534D4B34 => 4, //SMK4
+            _ => 0 // null, will never happen in a valid file
+            
+        };
     }
     
     public struct HuffmanFlag
