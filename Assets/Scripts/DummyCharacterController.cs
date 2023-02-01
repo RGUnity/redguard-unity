1656eb7b67c6890c609c363623b231b3928b4c34
Thane5 <13683581+Thane5@users.noreply.github.com>
2023-02-01 14:57:48 +0100

    Dummy character controller optimisations

--- Assets/Scripts/DummyCharacterController.cs
+++ Assets/Scripts/DummyCharacterController.cs
@@ -8,7 +8,24 @@ public class DummyCharacterController : MonoBehaviour
     public float moveSpeed = 10f;
     public bool useMouseLook;
     
+    
+    void FixedUpdate()
+    {
+        if (!GameManager.isGamePaused)
+        {
+            MovePlayer();
+        }
+    }
+    
     void Update()
+    {
+        if (!GameManager.isGamePaused)
+        {
+            RotatePlayer();
+        }
+    }
+
+    private void MovePlayer()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -17,30 +34,30 @@ public class DummyCharacterController : MonoBehaviour
         Vector3 zMove = transform.forward * z;
 
         
-        // Movement
         if (move.magnitude >= 0.1f && useMouseLook)
         {
             //Move the player along with X and Z inputs
-            controller.SimpleMove(move * (moveSpeed*30 * Time.deltaTime));
+            controller.SimpleMove(move * (moveSpeed * 30 * Time.deltaTime));
         }
 
         if (useMouseLook == false)
         {
             //Move the player only along the Z axis
-            controller.SimpleMove(zMove * (moveSpeed *30* Time.deltaTime));
+            controller.SimpleMove(zMove * (moveSpeed * 30 * Time.deltaTime));
         }
+    }
 
-        
-        // Rotation
+    private void RotatePlayer()
+    {
         if (useMouseLook)
         {
             //Rotate the player with the Mouse-X axis
-            transform.Rotate(0f,Input.GetAxis("Mouse X")*1.5f, 0f);
+            transform.Rotate(0f,Input.GetAxis("Mouse X")* Time.deltaTime *1.5f, 0f);
         }
         else
         {
             //use the Horizontal buttons to rotate the player
-            transform.Rotate(0f,Input.GetAxisRaw("Horizontal")*2, 0f);
+            transform.Rotate(0f,Input.GetAxisRaw("Horizontal")* Time.deltaTime*100, 0f);
         }
     }
 }
