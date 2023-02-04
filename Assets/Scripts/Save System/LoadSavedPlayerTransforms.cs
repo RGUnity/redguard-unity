6506118b96a4cfa032e97059e26ef5d5ceeaf0b0
Thane5 <13683581+Thane5@users.noreply.github.com>
2023-02-04 21:46:46 +0100

    Gave player option to spawn at first spawn point in sceneData

--- Assets/Scripts/Save System/LoadSavedPlayerTransforms.cs
+++ Assets/Scripts/Save System/LoadSavedPlayerTransforms.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ using UnityEditor;
 public class LoadSavedPlayerTransforms : MonoBehaviour
 {
     [SerializeField] private SceneData _sceneData;
+    [SerializeField] private bool useFirstSpawnPoint = false;
     [SerializeField] private Vector3 fallbackPosition = new (0,5,0);
 
     void Start()
@@ -28,23 +30,41 @@ public class LoadSavedPlayerTransforms : MonoBehaviour
         
         else
         {
-            // Then, try to load position & rotation from the savefile 
-            if (DataSerializer.HasKey("Player_Position") && DataSerializer.HasKey("Player_Rotation"))
-            {
-                Vector3 loadedPosition = DataSerializer.Load<Vector3>("Player_Position");
-                Quaternion loadedRotaion = DataSerializer.Load<Quaternion>("Player_Rotation");
-                SetPlayerPositionAndRotation(loadedPosition, loadedRotaion);
-                
-                print("Using Saved player transform data with position " + loadedPosition);
-            }
+            // "useFirstSpawnPoint" is primarily helpful for when we start a scene directly while in the Editor
+            // Because if you are in a scene, but the saveData is from another scene...
+            // ... you will spawn at the wrong position
             
-            // If nothing works, use the fallback vector as a position.
+            if (useFirstSpawnPoint)
+            {
+                Vector3 loadedPosition = _sceneData.playerSpawnPoints[0].position;
+                // Adding a small Y offset so that we dont fall through the floor
+                loadedPosition += new Vector3(0, 1, 0);
+                Quaternion loadedRotaion = Quaternion.Euler(_sceneData.playerSpawnPoints[0].eulerRotation);
+                SetPlayerPositionAndRotation(loadedPosition, loadedRotaion);
+            }
             else
             {
-                SetPlayerPositionAndRotation(fallbackPosition, new Quaternion());
+                // Then, try to load position & rotation from the savefile 
+                if (DataSerializer.HasKey("Player_Position") && DataSerializer.HasKey("Player_Rotation"))
+                {
+                    Vector3 loadedPosition = DataSerializer.Load<Vector3>("Player_Position");
+                    Quaternion loadedRotaion = DataSerializer.Load<Quaternion>("Player_Rotation");
+                    SetPlayerPositionAndRotation(loadedPosition, loadedRotaion);
+
+
+                    var sceneName = DataSerializer.Load<String>("CurrentScene");
+                    print("Loading player transform data for scene [" + sceneName + "] at position " + loadedPosition);
+                }
+            
+                // If nothing works, use the fallback vector as a position.
+                else
+                {
+                    SetPlayerPositionAndRotation(fallbackPosition, new Quaternion());
                 
-                print("No Entry point found. Using fallback position: " + fallbackPosition);
+                    print("No Entry point found. Using fallback position: " + fallbackPosition);
+                }
             }
+
         }
     }
 
