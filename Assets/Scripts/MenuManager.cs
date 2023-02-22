901e4f818c3c9cb6d53cadc276303c2c227bb603
Thane5 <13683581+Thane5@users.noreply.github.com>
2023-02-22 10:50:58 +0100

    Added Menu scene

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ContinueGame()
+    {
+        // Get Cached Scene, reload it
+    }
+    
+    public void ShowSavePage()
+    {
+        
+    }
+    
+    public void ShowLoadPage()
+    {
+        
+    }
+    
+    public void ShowOptionsPage()
+    {
+        
+    }
+    
+    public void ShowMoviesPage()
+    {
+        
+    }
+    
+    public void QuitGame()
+    {
+        
+    }
+}
