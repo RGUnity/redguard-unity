7b324c8d9ef25ba3a345fa41399538652ce35748
Thane5 <13683581+Thane5@users.noreply.github.com>
2023-03-04 09:52:50 +0100

    Menu Scene progress

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,15 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject saveMenu;
     
     // Start is called before the first frame update
     void Start()
     {
+        saveMenu.SetActive(false);
         
+        // if no game is loaded, disable "Continue" game
     }
 
     // Update is called once per frame
@@ -21,16 +27,32 @@ public class MenuManager : MonoBehaviour
     public void ContinueGame()
     {
         // Get Cached Scene, reload it
+        for (int i = 0; i < 19; i++)
+        {
+            var filePath = Application.persistentDataPath + "/Save_" + i + ".data";
+
+            if (File.Exists(filePath))
+            {
+                print("found file Save_" + i);
+            }
+        }
+        print(DateTime.Now.ToBinary());
     }
-    
+
     public void ShowSavePage()
     {
-        
+        saveMenu.SetActive(true);
+        mainMenu.SetActive(false);
     }
     
     public void ShowLoadPage()
     {
         
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("Interaction Tests");
     }
     
     public void ShowOptionsPage()
