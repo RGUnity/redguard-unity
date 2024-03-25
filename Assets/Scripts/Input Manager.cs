8b536793448a0e452d069e78b74ff742c6063409
Thane5 <13683581+Thane5@users.noreply.github.com>
2024-03-25 17:37:13 +0100

    Completed migration to new Input System package

--- Assets/Scripts/Input Manager.cs
+++ Assets/Scripts/Input Manager.cs
@@ -8,15 +8,26 @@ public class InputManager : MonoBehaviour
     [SerializeField] private PauseMenuLoader _pauseMenuLoader;
 
     public Vector2 move;
-    public bool jump;
     public bool moveModifier;
     public bool use;
+    public bool jump;
+    public bool toggleInventory;
+    public bool toggleMap;
+    public bool quickSave;
+    public bool quickLoad;
+    public bool togglePauseMenu;
     
     // Input System Actions
-    private InputAction moveAction;
-    private InputAction jumpAction;
-    private InputAction moveModifierAction;
-    private InputAction useAction;
+    private InputAction _moveAction;
+    private InputAction _moveModifierAction;
+    
+    private InputAction _useAction;
+    private InputAction _jumpAction;
+    private InputAction _toggleInventoryAction;
+    private InputAction _toggleMapAction;
+    private InputAction _quickSaveAction;
+    private InputAction _quickLoadAction;
+    private InputAction _togglePauseMenuAction;
     
     private UIWindowManager UiWindowManager;
     
@@ -27,46 +38,56 @@ public class InputManager : MonoBehaviour
         UiWindowManager = FindFirstObjectByType<UIWindowManager>();
         
         // Find input actions
-        moveAction = InputSystem.actions.FindAction("Move");
-        jumpAction = InputSystem.actions.FindAction("Jump");
-        moveModifierAction = InputSystem.actions.FindAction("Move Modifier");
-        useAction = InputSystem.actions.FindAction("Use");
+        _moveAction = InputSystem.actions.FindAction("Move");
+        _moveModifierAction = InputSystem.actions.FindAction("Move Modifier");
+        
+        _jumpAction = InputSystem.actions.FindAction("Jump");
+        _useAction = InputSystem.actions.FindAction("Use");
+        _toggleInventoryAction = InputSystem.actions.FindAction("Toggle Inventory");
+        _toggleMapAction = InputSystem.actions.FindAction("Toggle Map");
+        _quickSaveAction = InputSystem.actions.FindAction("Quick Save");
+        _quickLoadAction = InputSystem.actions.FindAction("Quick Load");
+        _togglePauseMenuAction = InputSystem.actions.FindAction("Toggle Pause Menu");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Quick Save"))
+        // Read input signals
+        move = _moveAction.ReadValue<Vector2>();
+        moveModifier = _moveModifierAction.IsPressed();
+        
+        use = _useAction.WasPressedThisFrame();
+        jump = _jumpAction.WasPressedThisFrame();
+        toggleInventory = _toggleInventoryAction.WasPressedThisFrame();
+        toggleMap = _toggleMapAction.WasPressedThisFrame();
+        quickSave = _quickSaveAction.WasPressedThisFrame();
+        quickLoad = _quickLoadAction.WasPressedThisFrame();
+        
+        if (quickSave)
         {
             print("Input Button Called: Quick Save");
             GameSaver.QuickSave();
         }
-        if (Input.GetButtonDown("Quick Load"))
+        if (quickLoad)
         {
             print("Input Button Called: Quick Load");
             GameLoader.QuickLoad();
         }
         
-
         if (!Game.Menu.isLoadedAdditively)
         {
             // Inputs that can only happen when the additive pause menu is HIDDEN 
 
-            if (Input.GetButtonDown("Fire2"))
+            if (toggleInventory)
             {
                 UiWindowManager.ToggleInventory();
             }
 
-            if (Input.GetButtonDown("Pause"))
+            if (togglePauseMenu)
             {
                 _pauseMenuLoader.ShowMainMenu();
             }
         }
-        
-        // Player Character Controls
-        move = moveAction.ReadValue<Vector2>();
-        jump = jumpAction.WasPressedThisFrame();
-        moveModifier = moveModifierAction.IsPressed();
-        use = useAction.WasPressedThisFrame();
     }
 }
