94b291610c0504b51aae34dec77e37bf51317ef3
Thane5 <13683581+Thane5@users.noreply.github.com>
2025-06-12 18:42:00 +0200

    add mv2 UI toggle

--- Assets/Scripts/ModelViewer2/ModelViewer2_Settings.cs
+++ Assets/Scripts/ModelViewer2/ModelViewer2_Settings.cs
@@ -7,6 +7,22 @@ public class ModelViewer2_Settings : MonoBehaviour
     [SerializeField] private ModelViewer2 mv2;
     [SerializeField] private ModelViewer2_GUI gui;
     [SerializeField] private ModelViewer2_Camera mv2_camera;
+    public bool showUI;
+
+    private void Start()
+    {
+        // Initialize showUI
+        showUI = gui.gameObject.activeSelf;
+    }
+
+    private void Update()
+    {
+        // ToggleUi Trigger
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            ToggleUI();
+        }
+    }
 
     public void ToggleFlyMode(bool toggle)
     {
@@ -17,5 +33,11 @@ public class ModelViewer2_Settings : MonoBehaviour
     {
         ModelLoader.RedguardPath = gui.pathInput.text;
         mv2.ViewerMode_Levels();
+    }
+
+    private void ToggleUI()
+    {
+        showUI = !showUI;
+        gui.gameObject.SetActive(showUI);
     }
 }
