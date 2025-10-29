70bc958ae944603c531366c89289bc8cd7b564dc
Thane5 <13683581+Thane5@users.noreply.github.com>
2025-10-29 11:12:45 +0100

    indentation fix

--- Assets/Scripts/ModelViewer/ModelViewer_GUI.cs
+++ Assets/Scripts/ModelViewer/ModelViewer_GUI.cs
@@ -29,51 +29,51 @@ public class ModelViewer_GUI : MonoBehaviour, IPointerEnterHandler, IPointerExit
     private List<GameObject> buttonList =  new();
     
     private Dictionary<string, string> fileToPalette = new()
-{
-    // Necromancer Isle, City Jail
-    ["DEAD.3DC"] = "NECRO",
-    ["JAILINT.ROB"] = "NECRO",
-    ["NECRISLE.ROB"] = "NECRO",
-    ["NECRTOWR.ROB"] = "NECRO",
-    ["NCROCK.3D"] = "NECRO",
-    ["SKELA001.3DC"] = "NECRO",
-    ["SKELA002.3DC"] = "NECRO",
-    ["SKELA003.3DC"] = "NECRO",
-    ["SKELA004.3DC"] = "NECRO",
-    ["VERMA001.3DC"] = "NECRO",
-    ["VERMA002.3DC"] = "NECRO",
-    ["VULTA001.3DC"] = "NECRO",
-    ["ZOMBA001.3DC"] = "NECRO",
-    ["ZOMBA002.3DC"] = "NECRO",
-    
-    // Goblin Caves, Mages Guild
-    ["CAVERNS.ROB"] = "REDCAVE",
-    ["CV_BOOM.3D"] = "REDCAVE",
-    ["CV_BOOM.3DC"] = "REDCAVE",
-    ["CV_EXPL1.3DC"] = "REDCAVE",
-    ["CV_MUSH2.3DC"] = "REDCAVE",
-    ["MGUILD.ROB"] = "REDCAVE",
-    
-    // Observatory, Dwemer Caves
-    ["DGOLA001.3DC"] = "OBSERVAT",
-    ["DRINT.ROB"] = "OBSERVAT",
-    ["ERASA001.3DC"] = "OBSERVAT",
-    ["GOLMA001.3DC"] = "OBSERVAT",
-    ["GOLMA002.3DC"] = "OBSERVAT",
-    ["OBSERVE.ROB"] = "OBSERVAT",
-    
-    // Restless League Hideout
-    ["FLAG_RL.3DC"] = "HIDEOUT",
-    ["HIDEINT.ROB"] = "HIDEOUT",
-    ["HIDEOUT.ROB"] = "HIDEOUT",
-    
-    // Imperial Palace
-    ["PALACE.ROB"] = "PALACE00",
-    ["PALATEST.ROB"] = "PALACE00",
-    
-    // Catacombs
-    ["CATACOMB.ROB"] = "CATACOMB"
-};
+    {
+        // Necromancer Isle, City Jail
+        ["DEAD.3DC"] = "NECRO",
+        ["JAILINT.ROB"] = "NECRO",
+        ["NECRISLE.ROB"] = "NECRO",
+        ["NECRTOWR.ROB"] = "NECRO",
+        ["NCROCK.3D"] = "NECRO",
+        ["SKELA001.3DC"] = "NECRO",
+        ["SKELA002.3DC"] = "NECRO",
+        ["SKELA003.3DC"] = "NECRO",
+        ["SKELA004.3DC"] = "NECRO",
+        ["VERMA001.3DC"] = "NECRO",
+        ["VERMA002.3DC"] = "NECRO",
+        ["VULTA001.3DC"] = "NECRO",
+        ["ZOMBA001.3DC"] = "NECRO",
+        ["ZOMBA002.3DC"] = "NECRO",
+        
+        // Goblin Caves, Mages Guild
+        ["CAVERNS.ROB"] = "REDCAVE",
+        ["CV_BOOM.3D"] = "REDCAVE",
+        ["CV_BOOM.3DC"] = "REDCAVE",
+        ["CV_EXPL1.3DC"] = "REDCAVE",
+        ["CV_MUSH2.3DC"] = "REDCAVE",
+        ["MGUILD.ROB"] = "REDCAVE",
+        
+        // Observatory, Dwemer Caves
+        ["DGOLA001.3DC"] = "OBSERVAT",
+        ["DRINT.ROB"] = "OBSERVAT",
+        ["ERASA001.3DC"] = "OBSERVAT",
+        ["GOLMA001.3DC"] = "OBSERVAT",
+        ["GOLMA002.3DC"] = "OBSERVAT",
+        ["OBSERVE.ROB"] = "OBSERVAT",
+        
+        // Restless League Hideout
+        ["FLAG_RL.3DC"] = "HIDEOUT",
+        ["HIDEINT.ROB"] = "HIDEOUT",
+        ["HIDEOUT.ROB"] = "HIDEOUT",
+        
+        // Imperial Palace
+        ["PALACE.ROB"] = "PALACE00",
+        ["PALATEST.ROB"] = "PALACE00",
+        
+        // Catacombs
+        ["CATACOMB.ROB"] = "CATACOMB"
+    };
 
     public bool IsMouseOverUI { get; private set; }
     
