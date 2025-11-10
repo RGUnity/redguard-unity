a6c5d519e171a16cc0ee32b6d65ad43b321ab5cf
Thane5 <13683581+Thane5@users.noreply.github.com>
2025-11-10 13:40:40 +0100

    Fix launcher window behavior on MacOS

--- Assets/Scripts/Core/Launcher.cs
+++ Assets/Scripts/Core/Launcher.cs
@@ -8,25 +8,23 @@ public class Launcher : MonoBehaviour
     [SerializeField] private DPIScaler scaler;
     [SerializeField] private string playScene;
     [SerializeField] private string modelViewerScene;
+    [SerializeField] private Vector2 baseWindowSize;
     
     private void Start()
     {
         float systemScale = scaler.GetScaleFactor();
         print("systemScale is " +  systemScale);
-        int scaledWidth = (int)(Screen.width * systemScale);
-        int scaledHeight = (int)(Screen.height * systemScale);   
-        
-        if (Application.platform == RuntimePlatform.WindowsPlayer)
-        {
-            // Apply system scale to window size resolution because Windows doesnt do that automatically
-            Screen.SetResolution(scaledWidth,  scaledHeight, FullScreenMode.Windowed);
-        }
-        
+        int scaledWidth = (int)(baseWindowSize.x * systemScale);
+        int scaledHeight = (int)(baseWindowSize.y * systemScale);   
+
+        // Apply system scale to window size
+        Screen.SetResolution(scaledWidth,  scaledHeight, FullScreenMode.Windowed);
+    
         // Move window to the center of screen (because we deleted a PlayerPrefs key) 
         List<DisplayInfo> displays = new List<DisplayInfo>();
         Screen.GetDisplayLayout(displays);
-        int width = Display.main.systemWidth/2 - scaledWidth/2;
-        int height = Display.main.systemHeight/2 - scaledHeight/2;
+        int width = Mathf.RoundToInt(Display.main.systemWidth/2 - scaledWidth/2);
+        int height = Mathf.RoundToInt(Display.main.systemHeight/2 - scaledHeight/2);
 
         Screen.MoveMainWindowTo(displays[0], new Vector2Int(width, height));
     }
@@ -68,6 +66,7 @@ public class Launcher : MonoBehaviour
     {
         // This ensures that the program starts using the window mode as it is defined in the project settings
         PlayerPrefs.DeleteKey("Screenmanager Fullscreen mode Default_h401710285");
+        PlayerPrefs.DeleteKey("Screenmanager Fullscreen mode Default");
     }
 
     private void OnApplicationQuit()
